ae3cf4fd37af6e2b25d29e47976917b034aa8018
Ibrahim Khalilullah <01754488189ib@gmail.com>
2026-06-25 15:47:09 +0600

    feat: map database collections and implement token authorization middleware

--- index.js
+++ index.js
@@ -28,6 +28,45 @@ client
   })
   .catch((err) => console.error("Database connection error:", err));
 
+const database = client.db(process.env.AUTH_DB_NAME || "b_13_assignment_10");
+const ebooksCollection = database.collection("ebooks");
+const usersCollection = database.collection("user");
+const sessionCollection = database.collection("session");
+const transactionsCollection = database.collection("transactions");
+const bookmarksCollection = database.collection("bookmarks");
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const session = await sessionCollection.findOne({ token: token });
+  if (!session) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const user =
+    (await usersCollection.findOne({ _id: session.userId })) ||
+    (await usersCollection.findOne({ id: session.userId }));
+
+  if (!user) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  if (user.status === "banned") {
+    return res.status(403).send({ message: "Your account has been banned." });
+  }
+
+  req.user = user;
+  next();
+};
+
 app.get("/", (req, res) => {
   res.send("Fable Platform Express Server Online");
 });
